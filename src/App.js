b1065a94358f88d5a5b27e601cbfee6a4e5d3491
Edgar Leon <eleon@haverford.edu>
2024-04-02 11:53:37 -0400

    Fixed Measure Range Params

--- src/App.js
+++ src/App.js
@@ -19,12 +19,13 @@ const App = () => {
     const emaExpression = urlParams.get("ema_expression");
     const fileName = pieceURL.split('/').pop();
     const newPieceURL = 'https://raw.githubusercontent.com/eleon024/ema_react_app/main/Music_Files/'+fileName;
-    const measure_range = JSON.parse(decodeURIComponent(urlParams.get("measure_range")));
+    // const measure_range = JSON.parse(decodeURIComponent(urlParams.get("measure_range")));
+    const measure_range = urlParams.get("measure_range");
     const observation = urlParams.get("observation");
     verovio.module.onRuntimeInitialized = async () => {
       const tk = new verovio.toolkit();
       tk.setOptions({
-        scale: 75,
+        scale: 50,
         adjustPageWidth: true,
       });
 
@@ -32,6 +33,10 @@ const App = () => {
         const response = await fetch(newPieceURL);
         const meiXML = await response.text();
         if (!response.ok) throw new Error("Failed to fetch MEI data.");
+        
+        // Construct the selection JSON object and stringify it
+        const selectionObject = { "measureRange": measure_range };
+        const selectionString = JSON.parse(JSON.stringify(selectionObject));
 
         // Extract and set title and composer from the MEI data
         const meiDoc = new DOMParser().parseFromString(meiXML, "text/xml");
@@ -45,8 +50,11 @@ const App = () => {
         const processor = EmaMei.withDocumentString(meiXML, emaExpression);
         const highlightedMei = processor.getSelection();
         const selectedIds = highlightedMei.querySelector("annot[type=ema_highlight]").getAttribute("plist");
-          tk.select(measure_range);
-          tk.redoLayout();
+          
+        tk.select(selectionString);
+        tk.redoLayout();
+
+        
         const svg = tk.renderData(new XMLSerializer().serializeToString(highlightedMei), {});
         setSvgContent(svg);
 
@@ -81,12 +89,12 @@ return (
       <h2><strong>Composer:</strong> {composer}<br /></h2>
       {observation && (
                 <div>
-                <h2><strong>Observation:</strong></h2>
-                {observation.split('\n').map((line, index) => (
+                <h2><strong>Observation:{observation}</strong></h2>
+                {/* {observation.split('\n').map((line, index) => (
                   // Using <div> here, but you can choose <span> or <p> depending on your styling needs
                   // The key is index which is sufficient here as the content is static
                   <p><div key={index}>{line}</div></p>
-                ))}
+                ))} */}
               </div>
       )}
     </div>
